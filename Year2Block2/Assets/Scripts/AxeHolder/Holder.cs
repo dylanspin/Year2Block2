49d92b84bc5af20bd06525cfe3882c57e1fb12a4
Dylan Spin <dylanspin100@hotmail.com>
2022-12-16 21:47:53 +0100

    Beginning of axe holding racks
    Need to fix a bug with it and then it works
    
    Need to fix the ragdoll collision ignoring

---
+++ Year2Block2/Assets/Scripts/AxeHolder/Holder.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Holder : MonoBehaviour
+{   
+    [Header("Settings")]
+
+    [Tooltip("The tag of the objects that can be held")]
+    [SerializeField] private string holdingTag = "Grabbable";
+
+    [Tooltip("If the object should follow the angle/position of the holder object")]
+    [SerializeField] private bool followSetPos = false;
+
+    [Header("Components")]
+
+    [Header("Optional")] [Tooltip("The gameobject that will be shown when in trigger")]
+    [SerializeField] private GameObject ghostIndicator;
+
+    [Header("Optional")] [Tooltip("The transform position where the item will be held")]
+    [SerializeField] private Transform holdingPoint;
+
+    [Header("Optional")] [Tooltip("Item that it should hold from the start")]
+    [SerializeField] private ItemHold startHold;
+
+    [Header("Private data")]
+
+    private bool holding = false;
+
+    ///still show a see trough indicator that the axe can be held maybe in a axe shape when in trigger
+
+    /// <summary>
+    /// Sets the start hold items
+    /// </summary>
+    private void Start()
+    {
+        if(startHold)
+        {
+            startHold.transform.parent = null;
+            startHold.setStartGrab(this);
+        }
+    }
+
+    /// <summary>
+    /// OnTriggerEnter is called when the Collider other enters the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        checkTrigger(other,true);
+    }
+
+    /// <summary>
+    /// OnTriggerExit is called when the Collider other has stopped touching the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    private void OnTriggerExit(Collider other)
+    {
+        checkTrigger(other,false);
+    }
+
+    /// <summary>
+    /// Checks if the trigger object is a grabable object and the sets the intrigger on that item to true or false
+    /// </summary>
+    private void checkTrigger(Collider other,bool active)
+    {
+        GameObject rootObject = other.transform.root.gameObject;
+        if(rootObject.tag == "Grabbable")
+        {
+            if(rootObject.GetComponent<ItemHold>())
+            {
+                ItemHold itemScript = rootObject.GetComponent<ItemHold>();
+                itemScript.setIntrigger(this,active);
+                showIndicator(active);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Shows or hides indicator object when in or out of the trigger 
+    /// </summary>
+    private void showIndicator(bool active)
+    {
+        if(!holding)
+        {
+            if(ghostIndicator)
+            {
+                ghostIndicator.SetActive(active);
+            }
+        }
+    }
+
+    /// <summary>
+    /// if item is set to be hold 
+    /// </summary>
+    public void setHold(bool active)
+    {
+        holding = active;
+    }
+
+    /// <summary>
+    /// returns if this holder is holding something
+    /// </summary>
+    public bool getHold()
+    {
+        return holding;
+    }
+
+    /// <summary>
+    /// returns custom grab transform pos or the current transform when the custom is not set
+    /// </summary>
+    public Transform getSetTrans()
+    {
+        return holdingPoint ? holdingPoint : transform;
+    }
+}
---
+++ Year2Block2/Assets/Scripts/AxeHolder/ItemHold.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemHold : MonoBehaviour
+{
+    [SerializeField] private MeshRenderer testMesh;
+    [SerializeField] private Material[] testMat;
+    [SerializeField] private Rigidbody objectRb;
+
+    private List<Holder> inTriggerList = new List<Holder>();
+    private Holder holderScript = null;
+    private bool grabbed = false;
+
+    /// <summary>
+    /// Sets if the object should be held as starting holding object
+    /// </summary>
+    public void setStartGrab(Holder startScript)
+    {
+        grabbed = true;
+        holderScript = startScript;
+        holderScript.setHold(true);
+        setHold(true);
+    }
+
+    /// <summary>
+    /// When the item is grabbed or let go by the interactor
+    /// </summary>
+    public void setGrab(bool active)
+    {
+        grabbed = active;
+        testMesh.material = active ? testMat[0] : testMat[1];
+        if(!active)
+        {
+            Holder closedPos = getClosed();
+            if(closedPos != null)
+            {
+                holderScript = closedPos;
+                holderScript.setHold(true);
+                setHold(true);
+            }
+        }
+        else
+        {
+            if(holderScript)
+            {
+                holderScript.setHold(false);
+                setHold(false);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Sets if the object should be held by the holder
+    /// </summary>
+    private void setHold(bool active)
+    {
+        if(active)
+        {
+            objectRb.constraints = RigidbodyConstraints.FreezeAll;
+            
+            Transform holdTrans = holderScript.getSetTrans();
+            transform.position = holdTrans.position;
+            transform.eulerAngles = holdTrans.eulerAngles;
+        }
+        else
+        {
+            objectRb.constraints = RigidbodyConstraints.None;
+        }
+    }
+
+    /// <summary>
+    /// Adds or removes holder to the script depending on if this object is in the trigger
+    /// </summary>
+    public void setIntrigger(Holder newAdd,bool add)
+    {
+        if(!inTriggerList.Contains(newAdd))
+        {
+            if(add)
+            {
+                inTriggerList.Add(newAdd);
+            }
+            else
+            {
+                inTriggerList.Remove(newAdd);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Function returns the most close transform in the list
+    /// </summary>
+    private Holder getClosed()
+    {
+        Holder closedPos = null;
+        float minDist = Mathf.Infinity;
+        Vector3 currentPos = transform.position;
+        foreach (Holder pos in inTriggerList)
+        {
+            float dist = Vector3.Distance(pos.transform.position, currentPos);
+
+            if(dist < minDist && !pos.getHold())
+            {
+                closedPos = pos;
+                minDist = dist;
+            }
+        }
+        
+        return closedPos;
+    }
+
+}
